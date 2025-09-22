997271741b205176bc725b7821a5342484e75753
Albireo-B <45595618+Albireo-B@users.noreply.github.com>
2025-09-22 21:25:37 +0200

    Update [Day cycle] : Add night light

--- Scenes/Environment/WorldEnv/world_environment.gd
+++ Scenes/Environment/WorldEnv/world_environment.gd
@@ -2,6 +2,7 @@ extends WorldEnvironment
 
 @export var day_cycle_duration: float = 60.0
 @export var sun: DirectionalLight3D
+@export var moon: DirectionalLight3D
 
 var sky: Sky = environment.sky
 var current_time: float = 0.0
@@ -12,11 +13,16 @@ func _process(delta):
 	# Adjust sun rotation 
 	var sun_angle: float = lerpf(-90.0, 270.0, time_normalized)
 	sun.rotation_degrees = Vector3(sun_angle, 0.0, 0.0)
+	moon.rotation_degrees = Vector3(sun_angle + 180, 0.0, 0.0)
 	# Adjust brightness
 	var light_energy: float = cos(deg_to_rad(sun_angle + 90))
 	if light_energy < 0.0:
-		sun.light_energy = 0.1
+		sun.light_energy = 0.0
 		sun.shadow_enabled = false
+		moon.light_energy = lerpf(0.1, 2.0, -light_energy)
+		moon.shadow_enabled = true
 	else:
 		sun.light_energy = lerpf(0.1, 2.0, light_energy)
 		sun.shadow_enabled = true
+		moon.light_energy = 0.0
+		moon.shadow_enabled = false
